75f89c22c3054ca1125c1e7d37c65383f0c1c445
woodsandpop <132575779+woodsandpop@users.noreply.github.com>
2023-07-05 17:10:06 +0100

    Update heartsegapp.py

--- heartsegapp.py
+++ heartsegapp.py
@@ -12,7 +12,7 @@ import time
 def imageInput(src):
     
     if src == 'Upload your own Image':
-        image_file = st.file_uploader("Upload An Image", type=['png', 'jpeg', 'jpg'])
+        image_file = st.file_uploader("Upload an Heart Image", type=['png', 'jpeg', 'jpg'])
         col1, col2 = st.columns(2)
         if image_file is not None:
             img = Image.open(image_file)
